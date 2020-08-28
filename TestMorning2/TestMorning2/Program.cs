9879a9c9a4446c0e2ad2a11f1c9ffa363f8d3409
Jake-CP <54853151+Jake-CP@users.noreply.github.com>
2020-08-28 09:57:21 -0500

    FirstCommit

---
+++ TestMorning2/TestMorning2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestMorning2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
